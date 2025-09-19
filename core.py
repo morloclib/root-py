9c3c689717304d2b6960aa272933c27b04771079
Zebulun Arendsee <zbwrnz@gmail.com>
2025-09-18 20:50:34 -0400

    Add idpy

--- core.py
+++ core.py
@@ -4,89 +4,92 @@
 import time
 import copy
 
-def mlc_elem(x, xs):
+def morloc_idpy(x):
+    return x
+
+def morloc_elem(x, xs):
     return x in xs
 
-def mlc_run(f):
+def morloc_run(f):
     return f()
 
-def mlc_neg(x):
+def morloc_neg(x):
   return (-1) * x
 
-def mlc_id(x):
+def morloc_id(x):
   return x
 
-def mlc_at(i, xs):
+def morloc_at(i, xs):
   return xs[i]
 
-def mlc_ifelse(cond, x, y):
+def morloc_ifelse(cond, x, y):
     if cond:
         return(x)
     else:
         return(y)
 
-def mlc_enumerateWith(f, xs):
+def morloc_enumerateWith(f, xs):
   for (x,i) in enumerate(xs):
     yield f(x,i)
 
-def mlc_fold(f, b, xs):
+def morloc_fold(f, b, xs):
   for x in xs:
     b = f(b, x)
   return b
 
-def mlc_add(a,b):
+def morloc_add(a,b):
   return a+b
 
-def mlc_sub(a,b):
+def morloc_sub(a,b):
   return a-b
 
-def mlc_mul(a,b):
+def morloc_mul(a,b):
   return a*b
 
-def mlc_div(a,b):
+def morloc_div(a,b):
   return a/b
 
-def mlc_map(f, *args):
+def morloc_map(f, *args):
   return list(map(f, *args))
 
-def mlc_append(xs, x):
+def morloc_append(xs, x):
     xs.append(x)
     return xs
 
-def mlc_filter(f, xs):
+def morloc_filter(f, xs):
     return [x for x in xs if f(x)]
 
-def mlc_fst(x):
+def morloc_fst(x):
   return x[0]
 
-def mlc_snd(x):
+def morloc_snd(x):
   return x[1]
 
-def mlc_thr3(x):
+def morloc_thr3(x):
   return x[2]
 
 #  onFst :: (a -> a') -> (a, b) -> (a', b)
-def mlc_onFst(f, x):
+def morloc_onFst(f, x):
     return (f(x[0]), x[1])
 
 #  onSnd :: (b -> b') -> (a, b) -> (a, b')
-def mlc_onSnd(f, x):
+def morloc_onSnd(f, x):
     return (x[0], f(x[1]))
 
 #  concat :: [[a]] -> [a]
-def mlc_concat(xss):
+def morloc_concat(xss):
     ys = []
     for xs in xss: 
         ys.extend(xs)
     return ys
 
 #  sleep py :: Real -> a -> a
-def mlc_sleep(n, a):
+def morloc_sleep(n, a):
     time.sleep(n)
     return a
 
 #  shard py :: Int -> [a] -> [[a]]
-def mlc_shard(chunkSize, xs):
+def morloc_shard(chunkSize, xs):
     xss = [[]]
     for x in xs:
         if len(xss[-1]) < chunkSize:
@@ -96,88 +99,88 @@ def mlc_shard(chunkSize, xs):
     return xss
 
 #  [a] -> a
-def mlc_head(xs):
+def morloc_head(xs):
     if(len(xs) == 0):
         raise ValueError("Empty list in head operation")
     else:
         return xs[0]
 
 # [a]_{n} -> [a]_{n-1}
-def mlc_tail(xs):
+def morloc_tail(xs):
     if(len(xs) == 0):
         raise ValueError("Empty list in tail operation")
     else:
         return xs[1:]
 
 # [a] -> a
-def mlc_last(xs):
+def morloc_last(xs):
     if(len(xs) == 0):
         raise ValueError("Empty list in last operation")
     else:
         return xs[-1]
 
 # i:Int -> [a]_{n>i} -> [a]_{n=i}
-def mlc_take(i, xs): 
+def morloc_take(i, xs): 
     return xs[0:i]
 
 # i:Int -> [a]_{n; n>i} -> [a]_{m; m <= n-i}
-def mlc_drop(i, xs):
+def morloc_drop(i, xs):
     return xs[i:]
 
 #  [a]_{n>i} -> [a]_{n-i}
-def mlc_init(xs):
+def morloc_init(xs):
     if(len(xs) == 0):
         raise ValueError("Empty list in init operation")
     else:
         return xs[0:-1]
 
 #  join py :: [a] -> [a] -> [a]
-def mlc_join(xs, ys):
+def morloc_join(xs, ys):
     # this function should not mutate the data
     xsCopy = copy.copy(xs)
     xsCopy.extend(ys)
     return xsCopy
 
-def mlc_filterKey(cond, d):
+def morloc_filterKey(cond, d):
     return {k:v for (k, v) in d.items() if cond(k)}
 
-def mlc_filterVal(cond, d):
+def morloc_filterVal(cond, d):
     return {k:v for (k, v) in d.items() if cond(v)}
 
-def mlc_keys(d):
+def morloc_keys(d):
     return list(d.keys())
 
-def mlc_vals(d):
+def morloc_vals(d):
     return list(d.values())
 
-def mlc_gt(x, y):
+def morloc_gt(x, y):
 	return x > y
 
-def mlc_lt(x, y):
+def morloc_lt(x, y):
 	return x < y
 
-def mlc_ge(x, y):
+def morloc_ge(x, y):
 	return x >= y
 
-def mlc_le(x, y):
+def morloc_le(x, y):
 	return x <= y
 
-def mlc_eq(x, y):
+def morloc_eq(x, y):
 	return x == y
 
-def mlc_ne(x, y):
+def morloc_ne(x, y):
 	return x != y
 
-def mlc_not(x):
+def morloc_not(x):
 	return (not x)
 
-def mlc_and(x, y):
+def morloc_and(x, y):
 	return (x and y)
 
-def mlc_or(x, y):
+def morloc_or(x, y):
 	return (x or y)
 
-def mlc_readMap(filename):
+def morloc_readMap(filename):
     x = dict()
     with open(filename, "r") as f:
         for line in f.readlines():
@@ -185,8 +188,8 @@ def mlc_readMap(filename):
             x[k.strip()] = v.strip()
         return x
 
-def mlc_seq(a, b):
+def morloc_seq(a, b):
     return b
 
-def mlc_const(a, b):
+def morloc_const(a, b):
     return a
